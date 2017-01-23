bfd1c130d47be1e7376796550370c90fe322196b
itoh <s1hit77@gmail.com>
2017-01-24 01:35:09 +0900

    jest test

---
+++ __tests__/actions/medicine-test.js
@@ -0,0 +1,18 @@
+import * as actions from '../../actions'
+
+describe('actions', () => {
+  it('should create an action to add a medicine', () => {
+    //const sampleAction = {
+    //  id: 'TEST001',
+    //  name: 'sample medicine',
+    //  isVisible: true
+    //};
+
+    //const expectedAction = {
+    //  id: sampleAction.id,
+    //  name: sampleAction.name,
+    //  isVisible: sampleAction.isVisible
+    //};
+    //expect(actions.addMedicine(sampleAction)).toEqual(expectedAction)
+  })
+})
