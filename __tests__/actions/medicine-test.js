5eec4c88ca468c11ae7336f5cbb5e50ab265919b
itoh <s1hit77@gmail.com>
2017-01-24 12:12:04 +0900

    jest 実行エラーを解消

--- __tests__/actions/medicine-test.js
+++ __tests__/actions/medicine-test.js
@@ -1,18 +1,20 @@
+import React from 'react';
 import * as actions from '../../actions'
 
 describe('actions', () => {
   it('should create an action to add a medicine', () => {
-    //const sampleAction = {
-    //  id: 'TEST001',
-    //  name: 'sample medicine',
-    //  isVisible: true
-    //};
+    const sampleAction = {
+      id: 'TEST001',
+      name: 'sample medicine',
+      isVisible: true
+    };
 
-    //const expectedAction = {
-    //  id: sampleAction.id,
-    //  name: sampleAction.name,
-    //  isVisible: sampleAction.isVisible
-    //};
-    //expect(actions.addMedicine(sampleAction)).toEqual(expectedAction)
+    const expectedAction = {
+      type: actions.ADD_MEDICINE,
+      id: sampleAction.id,
+      name: sampleAction.name,
+      isVisible: sampleAction.isVisible
+    };
+    expect(actions.addMedicine(sampleAction)).toEqual(expectedAction)
   })
 })
